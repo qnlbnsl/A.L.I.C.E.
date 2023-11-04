17172b60fc55c25ff46b02bea4200ccc0751d582
qnlbnsl <qnlbnsl@gmail.com>
2023-11-04 17:23:35 +0000

    update server code with webrtc

--- src/stt_processor/audio_stream/media_recorder.py
+++ src/stt_processor/audio_stream/media_recorder.py
@@ -5,15 +5,20 @@ import time
 import numpy as np
 from queue import Queue
 
+
 class MediaRecorder:
-    def __init__(self, rate, channels, width):
+    def __init__(self, rate: int, channels: int, width: int, layout: str):
         self.rate = rate
         self.channels = channels
         self.width = width
+        self.layout = layout
         self.recording = False
         self.frames = []
         self._queue = Queue()
         self._thread = threading.Thread(target=self._record, daemon=True)
+
+    def is_recording(self):
+        return self.recording
 
     def start(self):
         self.recording = True
@@ -43,13 +48,24 @@ class MediaRecorder:
 
     def _save_to_file(self):
         filename = f"recording_{int(time.time())}.wav"
-        wf = wave.open(filename, 'wb')
+        wf = wave.open(filename, "wb")
         wf.setnchannels(self.channels)
         wf.setsampwidth(self.width)
         wf.setframerate(self.rate)
-        wf.writeframes(b''.join(self.frames))
+        wf.writeframes(b"".join(self.frames))
         wf.close()
         print(f"Saved: {filename}")
+
+    def start_recording(self):
+        self.recording = True
+        if not self._thread.is_alive():
+            self._thread.start()
+
+    def stop_recording(self):
+        self.recording = False
+        self._thread.join()
+        self._save_to_file()  # Save the remaining frames when stopping the recording
+
 
 # Usage example:
 # recorder = Recorder(rate=44100, channels=2, width=pyaudio.get_sample_size(pyaudio.paInt16))
