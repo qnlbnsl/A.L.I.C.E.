d7d97758465e6b74a6969032f7a5735de535a545
qnlbnsl <qnlbnsl@gmail.com>
2023-11-04 12:27:24 -0400

    update for webrtc

--- src/mic_array_streamer/webrtc_stream/webrtc.py
+++ src/mic_array_streamer/webrtc_stream/webrtc.py
@@ -1,16 +1,21 @@
 import asyncio
 import json
+from time import sleep
 import websockets
 from aiortc import RTCPeerConnection, RTCSessionDescription
 from webrtc_stream.system_mic import SystemMic  # Assuming your SystemMic class is in system_mic.py
 
-async def rtc(pc, SIGNALING_SERVER_IP):
+from enums import RATE, CHANNELS
+
+async def rtc(pc: RTCPeerConnection, SIGNALING_SERVER_IP):
+    print("trying to connect over websockets...")
     # Assume we have a signaling channel that allows us to send and receive messages
     async with websockets.connect(f"ws://"+SIGNALING_SERVER_IP+":8080/ws") as ws:
+        print("connected over websockets!!!")
         # Add the SystemMic track to the peer connection
-        audio_track = SystemMic()
+        audio_track = SystemMic(rate=RATE, channels=CHANNELS, width=2, layout='7.1')
         pc.addTrack(audio_track)
-
+        pc.createDataChannel("meta")
         # Create an offer
         offer = await pc.createOffer()
         await pc.setLocalDescription(offer)
@@ -20,8 +25,11 @@ async def rtc(pc, SIGNALING_SERVER_IP):
         answer = json.loads(await ws.recv())
         await pc.setRemoteDescription(RTCSessionDescription(sdp=answer["sdp"], type=answer["type"]))
 
+        print("sleeping")
         # Wait for the connection to be established
-        await asyncio.sleep(30)
+        sleep(3)
 
+        print("stopping track")
         # Stop the audio track
         audio_track.stop()
+        print("stopped")
