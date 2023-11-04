d8837f8f139e6e8a7c91e217e5808fd2fff4d514
qnlbnsl <qnlbnsl@gmail.com>
2023-11-04 17:23:35 +0000

    update server code with webrtc

--- src/stt_processor/audio_stream/server_audio_track.py
+++ src/stt_processor/audio_stream/server_audio_track.py
@@ -1,18 +1,33 @@
 import asyncio
 from aiortc import MediaStreamTrack
 
+from audio_stream.media_recorder import MediaRecorder
 
-# This will be our audio track which will receive audio from the client
+
 class ServerAudioTrack(MediaStreamTrack):
     kind = "audio"
 
-    def __init__(self):
+    def __init__(self, recorder: MediaRecorder):
         super().__init__()  # don't forget to call super!
+        self.recorder = recorder
         self.__queue = asyncio.Queue()
 
     async def recv(self):
         frame = await self.__queue.get()
+        # Convert the frame to raw bytes and add it to the recorder's queue
+        if frame:
+            # If this is the first frame, start recording
+            if not self.recorder.is_recording():
+                self.recorder.start_recording()
+            # Convert the frame to raw bytes and add it to the recorder's queue
+            self.recorder.add_frame(frame.to_bytes())
         return frame
 
     async def put_frame(self, frame):
         await self.__queue.put(frame)
+
+    # Call this method when the track ends
+    def stop(self):
+        super().stop()
+        # Notify the recorder to stop recording
+        self.recorder.stop_recording()
--- src/stt_processor/audio_stream/webrtc.py
+++ src/stt_processor/audio_stream/webrtc.py
@@ -3,11 +3,8 @@ import json
 from aiohttp import web
 
 from logger import logger
-
-# import websockets
 from aiortc import (
     RTCPeerConnection,
-    MediaStreamTrack,
     RTCSessionDescription,
     RTCConfiguration,
     RTCIceServer,
@@ -26,54 +23,63 @@ async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
 
-    # Assume we only have one peer connection for simplicity
     pc = RTCPeerConnection(
         RTCConfiguration([RTCIceServer(urls="stun:stun.l.google.com:19302")])
     )
-    audio_track = ServerAudioTrack()
-    recorder = MediaRecorder(rate=44100, channels=8, width=2)
+    # Initialize the MediaRecorder with appropriate audio settings
+    recorder = MediaRecorder(rate=44100, channels=8, width=2, layout="7.1")
+    # Start the recorder
+    recorder.start()
 
-    @pc.on("datachannel")
-    def on_datachannel(channel):
-        @channel.on("message")
-        async def on_message(message):
-            logger.info("Received message:", message)
-            if isinstance(message, str) and message.startswith("ping"):
-                await channel.send("pong" + message[4:])
+    # Create a ServerAudioTrack and pass the recorder to it
+    audio_track = ServerAudioTrack(recorder=recorder)
+    # Add the ServerAudioTrack to the peer connection
+    pc.addTrack(audio_track)
 
     @pc.on("track")
     def on_track(track):
         logger.info(f"Received track {track.kind}")
-        pc.addTrack(audio_track)
 
         @track.on("ended")
         async def on_ended():
             logger.info(f"Track {track.kind} ended")
-            # await recorder.stop()
+            # Stop the recorder when the track ends
+            audio_track.stop()
+            # Renegotiate the connection
+            await renegotiate()
 
-    # Handle offer from the client
+    async def renegotiate():
+        # Create a new offer and send it to the remote peer
+        offer = await pc.createOffer()
+        await pc.setLocalDescription(offer)
+        await ws.send_str(
+            json.dumps(
+                {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+            )
+        )
+
+    # Handle offer/answer exchange and ICE candidates
     async for message in ws:
         data = json.loads(message.data)
         if "sdp" in data:
+            # ... existing SDP handling code ...
             offer = RTCSessionDescription(sdp=data["sdp"], type=data["type"])
             await pc.setRemoteDescription(offer)
-
-            # Prepare local media
-            # await recorder.start()
 
             # Send answer
             answer = await pc.createAnswer()
             if answer is None:
-                continue
+                logger.debug("Answer is None")
+                return
             await pc.setLocalDescription(answer)
             await ws.send_str(
                 json.dumps(
                     {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
                 )
             )
-
         elif "candidate" in data:
-            pass  # Handle ICE candidates (not shown here)
+            # ... existing ICE candidate handling code ...
+            pass
 
     return ws
 
@@ -87,3 +93,7 @@ def run_server():
     except KeyboardInterrupt:
         logger.info("Keyboard interrupt received. Exiting.")
         exit(0)
+
+
+if __name__ == "__main__":
+    run_server()
