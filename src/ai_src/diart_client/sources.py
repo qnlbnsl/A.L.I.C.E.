bea79432a96499d6fbe050a4d10ac9541208473a
qnlbnsl <qnlbnsl@gmail.com>
2023-11-07 13:23:31 -0500

    update new websocket stream

--- src/ai_src/diart_client/sources.py
+++ src/ai_src/diart_client/sources.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from queue import SimpleQueue
+from asyncio import Queue, Event, run_coroutine_threadsafe , get_running_loop, get_event_loop, AbstractEventLoop
+
 from typing import Text, Optional, Union, Tuple
 
 import numpy as np
@@ -57,55 +58,93 @@ class MicrophoneAudioSource(AudioSource):
 
     def __init__(
         self,
-        block_duration: float = 0.5,
+        block_duration: float, # Seconds
+        loop: AbstractEventLoop,
         device: Optional[Union[int, Text, Tuple[int, Text]]] = None,
     ):
-        # Use the lowest supported sample rate
+        self.loop = loop or get_event_loop()
+        # Use the highest? supported sample rate
         sample_rates = [16000, 32000, 44100, 48000]
         best_sample_rate = None
         for sr in sample_rates:
             try:
                 sd.check_input_settings(device=device, samplerate=sr)
             except Exception:
+                print("exception")
                 pass
             else:
                 best_sample_rate = sr
-                break
+                # break # this would break on the first hit....
         if best_sample_rate is None:
             best_sample_rate = sample_rates[2]
+            
+        print(f"Input Device: {device} with a sample rate of: {best_sample_rate}")
         super().__init__(f"input_device:{device}", best_sample_rate)
-
+        
         # Determine block size in samples and create input stream
         self.block_size = int(np.rint(block_duration * self.sample_rate))
+        print("Block Size: ", self.block_size)
+        
+        self._queue = Queue()
+        self._stream_started = Event()
+        
+        print("Initializing Input Stream")
         self._mic_stream = sd.InputStream(
             channels=8,
             samplerate=self.sample_rate,
-            latency=0,
+            latency='low',
             blocksize=self.block_size,
             callback=self._read_callback,
             device=device,
+            dtype=np.float32
         )
-        self._queue = SimpleQueue()
 
-    def _read_callback(self, samples, *args):
-        self._queue.put_nowait(samples[:, [0]].T)
+    def _read_callback(self, indata, frames, time, status):
+        if status:
+            print("Status:", status)
+        run_coroutine_threadsafe(self._queue.put(indata.copy()), self.loop)
 
-    def read(self):
-        self._mic_stream.start()
-        while self._mic_stream:
+
+    async def read(self):
+        self.start()
+        while not self._mic_stream.stopped:
             try:
-                while self._queue.empty():
-                    if self._mic_stream.closed:
-                        break
-                self.stream.on_next(self._queue.get_nowait())
+                data = await self._queue.get()
+                if data is None:
+                    # This is the signal that the stream has ended
+                    break
+                self.stream.on_next(data)
             except Exception as e:
                 self.stream.on_error(e)
                 break
         self.stream.on_completed()
-        self.close()
 
-    def close(self):
+    async def close(self):
         self._mic_stream.stop()
         self._mic_stream.close()
+        await self._queue.put(None)  # Signal the end of the stream
+        
+    def start(self):
+        try:
+            self._mic_stream.start()
+        except Exception as e:
+            self.stream.on_error(e)
+            return False
+        finally:
+            return True        
+        
+    def __aiter__(self):
+        return self
 
-
+    async def __anext__(self):
+        if self._mic_stream.stopped:
+            raise StopAsyncIteration
+        try:
+            data = await self._queue.get()
+            if data is None:
+                # None is the signal to stop iteration
+                raise StopAsyncIteration
+            return data
+        except Exception as e:
+            self.stream.on_error(e)
+            raise StopAsyncIteration
