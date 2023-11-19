f60c5f11071919d1d15a4f0aec1f1b1949dc7a74
Kunal <qnlbnsl@gmail.com>
2023-11-19 18:30:47 -0500

    Refactor beamforming.py for DOA estimation and signal processing

--- src/client/beamforming.py
+++ src/client/beamforming.py
@@ -1,14 +1,81 @@
 import numpy as np
+from numpy.typing import NDArray
 from scipy.linalg import eigh
 from scipy.interpolate import interp1d
+from scipy.signal import butter, lfilter
 
-from enums import CHANNELS, CHUNK, mic_positions
+from matrix import set_leds
+
+from enums import CHANNELS, CHUNK, RATE, mic_positions
 
 from logger import logger
+from utils import find_closest_mic_by_angle
 
 
 # @profile
-def beamform_audio(audio_data: np.ndarray) -> np.ndarray:
+def generate_steering_vectors(mic_positions, thetas, phis, freq, speed_of_sound=343):
+    k = 2 * np.pi * freq / speed_of_sound
+    steering_vectors = np.exp(
+        1j
+        * k
+        * (
+            mic_positions
+            @ np.array(
+                [
+                    np.sin(phis) * np.cos(thetas),
+                    np.sin(phis) * np.sin(thetas),
+                    np.cos(phis),
+                ]
+            )
+        )
+    )
+    return steering_vectors
+
+
+# DOA estimation parameters
+freq = 3000  # Choose a frequency for DOA estimation, can be adapted
+num_angles = 180
+possible_thetas = np.linspace(0, 2 * np.pi, num_angles)  # 360 degrees
+possible_phis = np.linspace((np.pi / 2) * (-1), np.pi / 2, num_angles)
+steering_vectors = generate_steering_vectors(
+    mic_positions, possible_thetas, possible_phis, freq
+)
+
+silent_waveform = np.zeros(CHUNK, dtype=np.int16)  # Using int16 for 16-bit audio
+min_rms_threshold = np.finfo(
+    float
+).eps  # effectively 1e-15 # a default minimum rms threshold
+
+
+# Bandpass filter function
+def bandpass_filter(data: np.ndarray, fs: int, order: int = 5) -> np.ndarray:
+    lowcut = 300
+    highcut = 3400
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype="band")
+    filtered_data = np.asarray(lfilter(b, a, data))
+    return filtered_data
+
+
+def calculate_signal_strength(signal):
+    # Calculate the RMS of the signal
+    rms = np.sqrt(np.mean(signal**2))
+    # rms = max(rms, min_rms_threshold)  # effectively max(rms, 1e-15)
+
+    # Convert RMS to decibels
+    ref = 32768  # Reference amplitude for 16-bit audio
+    # Avoid log of zero by replacing zero with a very small number
+    rms = np.finfo(float).eps if rms == 0 else rms
+    strength_db = 20 * np.log10(rms / ref)
+    if np.isnan(strength_db):
+        strength_db = min_rms_threshold
+    return strength_db
+
+
+# @profile
+def beamform_audio(audio_data: NDArray[np.int16]) -> NDArray[np.int16]:
     # Current shape of audio is interleaved 8 channels of block_duration * sample_rate
     # Reshape audio to an array of [CHANNEL][CHUNK]. This way each chunk is it's own channel.
     reshaped_audio_data = None
@@ -24,11 +91,20 @@ def beamform_audio(audio_data: np.ndarray) -> np.ndarray:
         # print(reshaped_audio_data.shape)  # Should print (8, 2048)
 
     # logger.debug("Estimating DOA")
-    theta, phi = estimate_doa_with_music(
-        audio_data=reshaped_audio_data, mic_positions=mic_positions, sampling_rate=16000
-    )
+    theta, phi = estimate_doa_with_music(audio_data=reshaped_audio_data)
+    # no return
     delays = calculate_delays(mic_positions, theta, phi, speed_of_sound=343, fs=16000)
-    return delay_and_sum(reshaped_audio_data, delays, 16000)
+    beamformed_signal = delay_and_sum(reshaped_audio_data, delays, 16000)
+    strength = calculate_signal_strength(beamformed_signal)
+    # in db The higher the number, the louder the sound. 0 is the loudest. -45 is the threshold.
+    if strength > -48:
+        # Limit calculated strength to -45 dB
+        find_closest_mic_by_angle(mic_positions, theta, phi, strength)
+        filtered_signal = bandpass_filter(beamformed_signal, fs=RATE)
+        set_leds(theta, strength)
+        return filtered_signal.astype(np.int16)
+    else:
+        return silent_waveform
 
 
 # @profile
@@ -47,7 +123,7 @@ def delay_and_sum(audio_data_2d, delays, fs):
     for i, delay in enumerate(delays):
         # Create an interpolation object for the current channel
         interpolator = interp1d(
-            t, audio_data_2d[i, :], kind="linear", fill_value="extrapolate"
+            t, audio_data_2d[i, :], kind="linear", fill_value="extrapolate"  # type: ignore
         )
 
         # Calculate the new time points, considering the delay
@@ -90,43 +166,17 @@ def calculate_delays(mic_positions, theta, phi, speed_of_sound=343, fs=44100):
     # Convert angles to radians
     theta = np.radians(theta)
     phi = np.radians(phi)
-
     # Calculate the unit vector for the given angles
     unit_vector = np.array(
         [np.sin(theta) * np.cos(phi), np.sin(theta) * np.sin(phi), np.cos(theta)]
     )
-
     # Calculate the delays in seconds
     delays_in_seconds = np.dot(mic_positions, unit_vector) / speed_of_sound
-
     # Normalize the delays to be relative to the first microphone
     delays_in_seconds -= delays_in_seconds[0]
 
-    # Convert delays from seconds to samples by multiplying with the sampling rate
-    # delays_in_samples = delays_in_seconds * fs
-
     # return delays_in_samples
     return delays_in_seconds
-
-
-# @profile
-def generate_steering_vectors(mic_positions, thetas, phis, freq, speed_of_sound=343):
-    k = 2 * np.pi * freq / speed_of_sound
-    steering_vectors = np.exp(
-        1j
-        * k
-        * (
-            mic_positions
-            @ np.array(
-                [
-                    np.sin(phis) * np.cos(thetas),
-                    np.sin(phis) * np.sin(thetas),
-                    np.cos(phis),
-                ]
-            )
-        )
-    )
-    return steering_vectors
 
 
 # @profile
@@ -149,24 +199,12 @@ def music_algorithm(R, steering_vectors, num_sources, num_angles):
 
 
 # @profile
-def estimate_doa_with_music(audio_data, mic_positions, sampling_rate):
-    freq = 1000  # Choose a frequency for DOA estimation, can be adapted
-    speed_of_sound = 343  # Speed of sound in air in m/s
-
+def estimate_doa_with_music(audio_data):
     # Create the spatial covariance matrix
     R = audio_data @ audio_data.conj().T / audio_data.shape[1]
 
-    # Generate possible DOA angles
-    num_angles = 180
-    possible_thetas = np.linspace(0, np.pi, num_angles)
-    possible_phis = np.linspace(0, np.pi / 2, num_angles)
-    steering_vectors = generate_steering_vectors(
-        mic_positions, possible_thetas, possible_phis, freq
-    )
-
     # Run the MUSIC algorithm
     pseudo_spectrum = music_algorithm(R, steering_vectors, 1, num_angles)
-
     # Find the angles corresponding to the peak
     estimated_theta = possible_thetas[np.argmax(pseudo_spectrum)]
     estimated_phi = possible_phis[np.argmax(pseudo_spectrum)]
