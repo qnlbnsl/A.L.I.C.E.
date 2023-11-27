6918cbeee802f295786cb39c5ddd0065867252f4
Kunal <qnlbnsl@gmail.com>
2023-11-27 18:32:56 -0500

    Refactor send_audio function to use asyncio and multiprocessing.Queue

--- src/client/sender.py
+++ src/client/sender.py
@@ -1,14 +1,15 @@
 import json
 import websockets
-
+import asyncio
+from multiprocessing import Queue
 from logger import logger
 
 
-def send_audio(ws: websockets.WebSocketClientProtocol, encoded_audio_queue):
+def send_audio(ws: websockets.WebSocketClientProtocol, encoded_audio_queue: Queue):
     while True:
-        logger.debug("Getting audio data from queue")
-        audio_data = encoded_audio_queue.get()
-        logger.debug("Sending audio data to server")
-        ws.send(json.dumps({"type": "audio", "data": audio_data}))
-        logger.debug("Audio data sent to server")
-        encoded_audio_queue.task_done()
+        # logger.debug("Getting audio data from queue")
+        audio_data = encoded_audio_queue.get(block=True, timeout=None)
+        logger.debug(f"Sending audio data to server with len: {len(audio_data)}")
+
+        asyncio.run(ws.send(json.dumps({"type": "audio", "data": audio_data})))
+        # logger.debug("Audio data sent to server")
