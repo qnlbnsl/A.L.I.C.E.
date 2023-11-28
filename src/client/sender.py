b790384592255c707f2097d443586b51896d57ff
Kunal <qnlbnsl@gmail.com>
2023-11-27 19:34:50 -0500

    Commented out debug statement in send_audio function

--- src/client/sender.py
+++ src/client/sender.py
@@ -9,7 +9,7 @@ def send_audio(ws: websockets.WebSocketClientProtocol, encoded_audio_queue: Queu
     while True:
         # logger.debug("Getting audio data from queue")
         audio_data = encoded_audio_queue.get(block=True, timeout=None)
-        logger.debug(f"Sending audio data to server with len: {len(audio_data)}")
+        # logger.debug(f"Sending audio data to server with len: {len(audio_data)}")
 
         asyncio.run(ws.send(json.dumps({"type": "audio", "data": audio_data})))
         # logger.debug("Audio data sent to server")
