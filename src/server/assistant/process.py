11c399aa4e051053e7a2c815bfedaa83070e29b6
qnlbnsl <qnlbnsl@gmail.com>
2023-11-29 18:43:55 +0000

    remove spacy and use bert

--- src/server/assistant/process.py
+++ src/server/assistant/process.py
@@ -3,12 +3,14 @@ import time
 from typing import List
 from logger import logger
 import re
-import spacy
+
 import csv
+from logger import logger
 
 from assistant.intents.parse_intent import intent_queue
 from assistant.concept_store.parse_concept import concept_queue
 from assistant.questions.parse_question import question_queue
+from text_classification.text_classification import classify_sentence
 
 transcribed_text_queue = asyncio.Queue()
 
@@ -30,103 +32,6 @@ def import_and_organize_data(file_path):
 RED = "\033[91m"
 UNDERLINE = "\033[4m"
 END = "\033[0m"
-data = import_and_organize_data("src/server/assistant/training_data/train.csv")
-# logger.debug(data)
-test_data = {
-    "question": [
-        "Can you recommend a good mystery novel?",
-        "Why do leaves change color in the fall?",
-        "How do you bake a chocolate cake?",
-        "What's the capital of France?",
-        "Who won the Nobel Prize in Physics last year?",
-        "How many planets are in our solar system?",
-        "What is the largest mammal on Earth?",
-        "How is the weather today?",
-        "How is the weather outside?",
-        "What is the stock price for tesla?",
-        "What are the current fashion week dates for Milan?",
-        "What are the fashion week dates for Milan this year?",
-        "What's the unemployment rate in the United States?",
-        "What are the daily recommended nutritional guidelines?",
-        "Who is the most followed person on Instagram at the moment?",
-        "When is the next SpaceX launch?",
-        "What is the current price of Bitcoin?",
-        "Are there any severe weather warnings in effect for Florida?",
-        "How much traffic is on the I-95 at this moment?",
-        "What is quantum computing?",
-        "How did IBM achieve quantum supremacy?",
-        "What is the difference between a quantum computer and a classical computer?",
-        "Can i mix bleach and ammonia?",
-    ],
-    "command": [
-        "Lock all the doors",
-        "Close the garage door",
-        "Turn on the porch light",
-        "Start the coffee maker",
-        "Activate the alarm system",
-        "Set the bedroom lights to warm white",
-        "Turn on the ceiling fan in the living room",
-        "Increase the fridge temperature by two degrees",
-        "Show me the backyard camera feed",
-        "Start recording on the security camera",
-        "Play my workout playlist in the home gym",
-        "Turn off all lights upstairs",
-        "Set a timer for 20 minutes for the oven",
-        "Pause the dishwasher cycle",
-        "Resume the robot vacuum cleaning",
-        "Turn on the holiday lighting scene",
-        "Activate the garden sprinklers",
-        "Set the hot tub temperature to 100 degrees",
-        "Turn on the dining room lights at 7 PM",
-        "Adjust the living room lights for reading",
-        "Can you please turn on the TV?",
-        "Can you please turn on the living room lights?",
-        "Can you please make me some coffee?",
-        "Can you please water the plants?",
-    ],
-    "other": [
-        "The omniscient narrator in literature sees and knows all, like a god of their created world",
-        "The Sapir-Whorf hypothesis argues that language determines thought",
-        "Cognitive dissonance is the mental discomfort experienced by holding contradictory beliefs",
-        "The Mandela effect is when a large group of people remembers something differently than how it occurred",
-        "The Overton window frames the range of policies politically acceptable to the mainstream at a given time",
-        "The paradox of thrift posits that individual savings can lead to a collective economic downturn",
-        "The law of diminishing returns states that successive increases in inputs yield progressively smaller increases in outputs",
-        "The Great Filter theory suggests there is a barrier to the development of advanced civilizations",
-        "The Fermi paradox ponders the apparent absence of extraterrestrial life despite its high probability",
-        "The concept of wabi-sabi embraces the beauty of imperfection and transience",
-        "The Peter principle states that people in a hierarchy tend to rise to their level of incompetence",
-        "The Baader-Meinhof phenomenon is the illusion that something newly learned suddenly appears everywhere",
-        "The principle of charity suggests interpreting others' statements in the most rational way possible",
-        "The narrative fallacy is the tendency to fit events into a story after they have happened",
-        "The concept of an omniverse encompasses every universe, multiverse, and possibility.",
-        "The Ship of Theseus paradox explores the nature of identity and change",
-        "The Rorschach test uses inkblots to delve into the subconscious mind",
-        "Autonomy in technology raises ethical questions about the role of human oversight",
-        "The paradox of tolerance highlights the need to limit tolerance to preserve an open society",
-        "Shadow work refers to unpaid labor, such as self-service checkout, that benefits companies.",
-        "The concept of a technological singularity reflects the point where AI surpasses human intelligence",
-        "Collective intelligence emerges when groups act in ways that seem intelligent",
-        "The Hawthorne effect is the alteration of behavior by study subjects due to their awareness of being observed",
-        "Of course.",
-        "You have 20 seconds left on your alarm.",
-        "You have 5 new notifications",
-        "Your package has been delivered.",
-        "What's going on?",  # Too vague to be a question.
-        "It's very popular.",
-    ],
-}
-
-
-nlp_v2 = spacy.blank("en")
-nlp_v2.add_pipe(
-    "classy_classification",
-    config={
-        "data": test_data,
-        "model": "sentence-transformers/all-mpnet-base-v2",
-        "device": "gpu",
-    },
-)
 
 
 class SegmentBuffer:
@@ -231,18 +136,6 @@ class SentenceBuffer:
         return False
 
 
-async def classify_sentence(sentence: str) -> str:
-    # logger.debug(f"Classifying sentence: {sentence}")
-    doc = nlp_v2(sentence)
-    # logger.debug(doc._.cats)
-    if doc._.cats:
-        # Find the category with the maximum score
-        category = max(doc._.cats, key=doc._.cats.get)  # type: ignore
-        return category
-    else:
-        return "Other"  # Return a default value if doc.cats is empty
-
-
 # Modify process_segments function to use the updated SentenceBuffer
 async def process_segments(timeout: float = 5.0):
     segment_buffer = SegmentBuffer()
@@ -250,15 +143,15 @@ async def process_segments(timeout: float = 5.0):
 
     while True:
         try:
-            segment = await asyncio.wait_for(transcribed_text_queue.get(), timeout)
+            segment = await transcribed_text_queue.get()
             cleaned_segment = re.sub(r"\.{2,}", ".", segment)
             # this should filter the period that keeps coming in as individual segments
             if len(cleaned_segment) <= 1:
                 continue
             segment_buffer.add_segment(cleaned_segment)
-            # logger.debug(f"segment buffer: {segment_buffer.buffer}")
-            # logger.debug(f"segment: {cleaned_segment}")
-            # logger.info(f"Added segment: {segment}")
+            logger.debug(f"segment buffer: {segment_buffer.buffer}")
+            logger.debug(f"segment: {cleaned_segment}")
+            logger.info(f"Added segment: {segment}")
             # if segment is small then skip classification and continue
             if len(cleaned_segment) <= 10:
                 logger.info(
@@ -271,7 +164,7 @@ async def process_segments(timeout: float = 5.0):
                 # sentence should be at least 10 characters long
                 if len(sentence) <= 15:
                     continue
-                classification = await classify_sentence(sentence)
+                classification = classify_sentence(sentence)
                 # run and forget
                 asyncio.create_task(
                     sentence_buffer.handle_classification(
@@ -292,7 +185,7 @@ async def process_segments(timeout: float = 5.0):
                     )
                     continue
                 logger.debug("classifying sentence: 2")
-                classification = await classify_sentence(sentence)
+                classification = classify_sentence(sentence)
                 asyncio.create_task(
                     sentence_buffer.handle_classification(
                         classification, sentence, reset=True
