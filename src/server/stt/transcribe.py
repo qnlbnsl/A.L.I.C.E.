96409928b2546d80245149be802b6e0eda1a10e7
qnlbnsl <qnlbnsl@gmail.com>
2023-12-03 20:58:03 +0000

    Update transcribe.py with WhisperModel changes

--- src/server/stt/transcribe.py
+++ src/server/stt/transcribe.py
@@ -1,29 +1,29 @@
 import numpy as np
 from numpy.typing import NDArray
 
-import torch
-import whisper
+# import torch
+# import whisper
 
-import faster_whisper.transcribe
+# import faster_whisper.transcribe
 from faster_whisper import WhisperModel
 
 from logger import logger
 
 whisper_model = "tiny.en"
-model_size = "large-v3"
+model_size = "/home/qnlbnsl/ai_voice_assistant/src/server/whisper-large-v3-ct2"
 device = "cuda"
-compute_type = "float32"
-model = whisper.load_model(whisper_model, device=device)
+compute_type = "int8_float16"
+# model = whisper.load_model(whisper_model, device=device)
 
 # faster_whisper.transcribe.Tokenizer.encode = lambda self, text: self.tokenizer.encode(
 #     text, add_special_tokens=False
 # )
 # # Initialize the model outside of the transcribe function
-# model = WhisperModel(
-#     model_size_or_path=model_size,
-#     device=device,
-#     compute_type=compute_type,
-# )
+model = WhisperModel(
+    model_size_or_path=model_size,
+    device=device,
+    compute_type=compute_type,
+)
 
 # # # Monkey patch 3 (change n_mels)
 # from faster_whisper.feature_extractor import FeatureExtractor
@@ -38,37 +38,30 @@ model = whisper.load_model(whisper_model, device=device)
 #     token
 # )
 
-
-# hard-coded audio hyperparameters
-N_FFT = 400
-HOP_LENGTH = 160
+word_timestamps = False
+initial_prompt = None
 
 
 # Function to run transcription in a separate thread
 def transcribe_chunk(audio_chunk: NDArray[np.float32]):
     try:
-        logger.debug(
-            f"Transcribing chunk of shape: {audio_chunk.shape}, type: {audio_chunk.dtype}"
-        )
-        # logger.debug("padding audio chunk")
-        # audio = whisper.pad_or_trim(audio_chunk)
-        # logger.debug("padding complete")
-        # logger.debug(f"loading mel spectrogram")
-        # mel = whisper.log_mel_spectrogram(audio).to(model.device)
-        # logger.debug(f"mel spectrogram loaded")
-        # logger.debug("Retrieving decoding options")
-        # options = whisper.DecodingOptions()
-
-        # logger.debug("Decoding audio")
-        # data = whisper.decode(model, mel, options)
-        data = model.transcribe(
+        # logger.debug(
+        #     f"Transcribing chunk of shape: {audio_chunk.shape}, type: {audio_chunk.dtype}"
+        # )
+        segments, info = model.transcribe(
             audio_chunk,
-            temperature=0.2,
-            fp16=torch.cuda.is_available(),
-            word_timestamps=True,
+            beam_size=10,
+            vad_filter=True,
+            word_timestamps=word_timestamps,
+            temperature=0.0,
+            language="en",
+            initial_prompt=initial_prompt,
         )
-        logger.debug(f"Transcribed data: {data}")
-        return data
+
+        for segment in segments:
+            logger.debug(f"Transcribed segment: {segment.text}")
+        # logger.debug(f"Transcribed data: {segments}")
+        # return data
     except Exception as e:
         logger.error(f"Error in transcribe_chunk: {e}")
         raise e
