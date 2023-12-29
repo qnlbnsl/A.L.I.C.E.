b9a5fd2b5fd020cee6d18743d2a9ffc462d28dca
qnlbnsl <qnlbnsl@gmail.com>
2023-12-29 23:13:16 +0000

    Refactor parse_question function and add answer_question function

--- src/server/assistant/questions/parse_question.py
+++ src/server/assistant/questions/parse_question.py
@@ -1,16 +1,19 @@
 from multiprocessing import Queue
 from multiprocessing.synchronize import Event
-import time
 
 from logger import logger
 
 
-def parse_question(shutdown_event: Event, question_queue: Queue, wake_word_event: Event):
-    while shutdown_event.is_set() is False and wake_word_event.is_set() is True:
+def parse_question(shutdown_event: Event, question_queue: Queue[str], wake_word_event: Event, response_event: Event) -> str | None:
+    while shutdown_event.is_set() is False:
         question = question_queue.get()
-        if question is None:
-            time.sleep(1)
-            continue
         logger.debug(question)
+        answer = answer_question(question)
+        wake_word_event.clear()
+        response_event.set()
+        return answer
         # question_queue.task_done()
-    wake_word_event.clear()
+    return None
+
+def answer_question(question: str) -> str:
+    return "I don't know"
