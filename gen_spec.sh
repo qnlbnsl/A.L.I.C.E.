92eeeb1e7483193ffa731d81080b9647c1d0b436
qnlbnsl <qnlbnsl@gmail.com>
2024-01-22 21:13:30 +0000

    Update package names and versions

--- gen_spec.sh
+++ gen_spec.sh
@@ -4,7 +4,7 @@ sudo docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli gener
     -i /local/openapi.json \
     -g python \
     -o /local/out/python \
-    --package-name arguflow \
+    --package-name trieve_python_client \
 
 sudo chown -hR $USER:$USER ./out/python
 # pip install ./out/python
